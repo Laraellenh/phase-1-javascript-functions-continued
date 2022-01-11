b6e5da5b2d276d44968874de6df3996aca2e5de6
Lara <laraellenh@gmail.com>
2022-01-11 11:16:55 -0500

    commit tree e69afe23ca9383209fc2c6ee48fadfbd6e1be9a0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+
+  function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+  }
+
+  function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+  }
+ 
+const wrapAdjective = function(style="*") {
+  return function(feeling="special") {
+    
+    return `You are ${style}${feeling}${style}!`
+  }
+}
